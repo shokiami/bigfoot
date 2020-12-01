44065c33c10c87d3aad622ff05cf752511c559f3
shokiami <kiami.sho@gmail.com>
2020-11-30 23:03:05 -0800

    Changed Shipping object to keep track of product.

--- Shipping.java
+++ Shipping.java
@@ -1,19 +1,22 @@
 import java.util.*;
 
 public class Shipping implements ProductTrait{
+    private Product product;
     private Map<String, Double> distances;
     private Map<String, Integer> ratios;
 
-    public Shipping(Map<String, Double> distances) {
-        this.distances = new HashMap<String, Double>(distances);
-        this.ratios = new HashMap<String, Integer>();
+    public Shipping(Product product, Map<String, Double> distances) {
+        this.product = product;
+        this.distances = new HashMap<>(distances);
+        this.ratios = new HashMap<>();
         ratios.put("road", 62);
         ratios.put("rail", 22);
         ratios.put("sea", 31);
         ratios.put("air", 602);
     }
 
-    public double estimate(Product product) {
+    // Returns kilograms of CO2
+    public double estimate() {
         double result = 0;
         for (String method : distances.keySet()) {
             result += ratios.get(method) * product.getWeight() * distances.get(method) / 1000000;
@@ -22,4 +25,4 @@ public class Shipping implements ProductTrait{
     }
 }
 
-//https://cefic.org/app/uploads/2018/12/MeasuringAndManagingCO2EmissionOfEuropeanTransport-McKinnon-24.01.2011-REPORT_TRANSPORT_AND_LOGISTICS.pdf
+// https://cefic.org/app/uploads/2018/12/MeasuringAndManagingCO2EmissionOfEuropeanTransport-McKinnon-24.01.2011-REPORT_TRANSPORT_AND_LOGISTICS.pdf
