01b3415cb50b69cf2682c8215985222d29188013
njbradley <>
2020-12-09 00:36:06 -0800

    fixed all tabs

--- Material.java
+++ Material.java
@@ -6,39 +6,39 @@
 import java.util.*;
 
 class Material {
-  private String category;
-  private String name;
-  private double co2PerKg;
-  
-	// fills in the material data from a file
-	// Format: unique-id category name kg-co2/kg-material
-  public Material(Scanner ifile) {
-    category = ifile.next();
-    name = ifile.next();
-    if (name.charAt(0) == '"') {
-      name = name.substring(1, name.length()-1);
+    private String category;
+    private String name;
+    private double co2PerKg;
+    
+  	// fills in the material data from a file
+  	// Format: unique-id category name kg-co2/kg-material
+    public Material(Scanner ifile) {
+        category = ifile.next();
+        name = ifile.next();
+        if (name.charAt(0) == '"') {
+            name = name.substring(1, name.length()-1);
+        }
+    		co2PerKg = ifile.nextDouble();
     }
-		co2PerKg = ifile.nextDouble();
-  }
-  
-  // constructs a material object with all of the attributes needed.
-  // category and name cannot be null.
-  public Material(String category, String name, double co2PerKg) {
-    this.category = category;
-    this.name = name;
-    this.co2PerKg = co2PerKg;
-  }
-  
-	// returns the carbon footprint in kg co2
-  public double estimate(double kg) {
-    return co2PerKg * kg;
-  }
-  
-  public String getName() {
-    return name;
-  }
-  
-  public String getCategory() {
-    return category;
-  }
+    
+    // constructs a material object with all of the attributes needed.
+    // category and name cannot be null.
+    public Material(String category, String name, double co2PerKg) {
+        this.category = category;
+        this.name = name;
+        this.co2PerKg = co2PerKg;
+    }
+    
+  	// returns the carbon footprint in kg co2
+    public double estimate(double kg) {
+        return co2PerKg * kg;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getCategory() {
+        return category;
+    }
 }
--- MaterialData.java
+++ MaterialData.java
@@ -4,30 +4,30 @@
 import java.util.*;
 
 class MaterialData {
-  private List<Material> allMats;
-  
-  // constructs the instance with a prefilled list of materials.
-  public MaterialData(List<Material> allMats) {
-    this.allMats = allMats;
-  }
-  
-	// fills the material list from a scanner, with lines of Material objects
-  public MaterialData(Scanner ifile) {
-    allMats = new ArrayList<Material>();
-    ifile = ifile.useDelimiter("\t|\r\n|\n");
-    ifile.nextLine();
-    while (ifile.hasNext()) {
-			allMats.add(new Material(ifile));
-		}
-  }
-  
-	// returns a material that matches the passed name
-  public Material getByName(String name) {
-    for (Material mat : allMats) {
-      if (mat.getName().equals(name) || mat.getCategory().equals(name)) {
-        return mat;
-      }
+    private List<Material> allMats;
+    
+    // constructs the instance with a prefilled list of materials.
+    public MaterialData(List<Material> allMats) {
+        this.allMats = allMats;
     }
-    return null;
-  }
+    
+  	// fills the material list from a scanner, with lines of Material objects
+    public MaterialData(Scanner ifile) {
+        allMats = new ArrayList<Material>();
+        ifile = ifile.useDelimiter("\t|\r\n|\n");
+        ifile.nextLine();
+        while (ifile.hasNext()) {
+      			allMats.add(new Material(ifile));
+    		}
+    }
+    
+  	// returns a material that matches the passed name
+    public Material getByName(String name) {
+        for (Material mat : allMats) {
+            if (mat.getName().equals(name) || mat.getCategory().equals(name)) {
+                return mat;
+            }
+        }
+        return null;
+    }
 }
